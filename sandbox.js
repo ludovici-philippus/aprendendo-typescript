4c245432f67f46386cf760f36409897ee40461f5
Luis Felipe <202makertv@gmail.com>
2022-03-02 11:46:36 -0300

    Arrays e Objetos

--- sandbox.js
+++ sandbox.js
@@ -1,7 +1,28 @@
-var character = "Mario";
-var age = 20;
-var isBlackBelt = false;
-function circunfence(diameter) {
-    return diameter * Math.PI;
-}
-console.log(circunfence(40));
+// arrays
+var names = ["luigi", "mario", "yoshi"];
+names.push("toad");
+console.log(names);
+var numbers = [10, 20, 30, 40];
+numbers.push(50);
+console.log(numbers);
+var mixed = ["Ken", 4, "Chun-Li", 8, 9];
+mixed.push("Ryu");
+mixed.push(10);
+console.log(mixed);
+// Objects
+var ninja = {
+    name: "Mario",
+    belt: "Black",
+    age: 30
+};
+console.log(ninja);
+ninja.age = 20;
+ninja.name = "Ryu";
+//ninja.age = '30' Can't be done;
+//ninja.skills = ["fighting", "sneaking"] Can't be done;
+ninja = {
+    name: "Yoshi",
+    belt: "Orange",
+    age: 40
+};
+// Can be done, whilst it keep the same properties from the original object;
--- sandbox.ts
+++ sandbox.ts
@@ -1,9 +1,40 @@
-let character: string = "Mario";
-let age: number = 20;
-let isBlackBelt: boolean = false;
+// arrays
+let names: string[] = ["luigi", "mario", "yoshi"]
 
-function circunfence (diameter: number) : number{
-  return diameter * Math.PI;
+names.push("toad");
+
+console.log(names);
+
+let numbers: number[] = [10, 20, 30, 40];
+
+numbers.push(50);
+
+console.log(numbers);
+
+let mixed = ["Ken", 4, "Chun-Li", 8, 9];
+
+mixed.push("Ryu");
+mixed.push(10);
+
+console.log(mixed)
+
+// Objects
+
+let ninja = {
+  name: "Mario",
+  belt: "Black",
+  age: 30
 }
 
-console.log(circunfence(40));
+console.log(ninja);
+ninja.age = 20;
+ninja.name = "Ryu";
+//ninja.age = '30' Can't be done;
+//ninja.skills = ["fighting", "sneaking"] Can't be done;
+
+ninja = {
+  name: "Yoshi",
+  belt: "Orange",
+  age: 40
+}
+// Can be done, whilst it keep the same properties from the original object;
