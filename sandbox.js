f2555c17d56f4441ca6d626d7940f1613afd8ab4
Luis Felipe <202makertv@gmail.com>
2022-03-02 11:39:57 -0300

    Primeiros passos com typescript

---
+++ sandbox.js
@@ -0,0 +1,7 @@
+var character = "Mario";
+var age = 20;
+var isBlackBelt = false;
+function circunfence(diameter) {
+    return diameter * Math.PI;
+}
+console.log(circunfence(40));
---
+++ sandbox.ts
@@ -0,0 +1,9 @@
+let character: string = "Mario";
+let age: number = 20;
+let isBlackBelt: boolean = false;
+
+function circunfence (diameter: number) : number{
+  return diameter * Math.PI;
+}
+
+console.log(circunfence(40));
