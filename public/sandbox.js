16af12c2f2442882053a18f41981e4bbabea8c1e
Luis Felipe <202makertv@gmail.com>
2022-03-04 12:59:07 -0300

    Aula 8

--- public/sandbox.js
+++ public/sandbox.js
@@ -1,2 +1,14 @@
 "use strict";
-console.log("test 123");
+let greet;
+greet = () => {
+    console.log("Hello, world!");
+};
+const add = (num1, num2, num3, num4 = 10) => {
+    return num1 + num2;
+};
+console.log(add(5, 10, 3));
+const minus = (a, b) => {
+    return a - b;
+};
+let result = minus(5, 3);
+console.log(result);
--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,1 +1,18 @@
-console.log("test 123");
+let greet: Function;
+
+greet = (): void => {
+  console.log("Hello, world!");
+}
+
+const add = (num1: number, num2: number, num3?: number | string, num4: number = 10) : number => { 
+  return num1 + num2;
+}
+
+console.log(add(5, 10, 3));
+
+const minus = (a: number, b: number) : number => {
+  return a - b;
+}
+
+let result = minus(5, 3);
+console.log(result);
