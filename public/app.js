7fa2e4c609ab665b82f4af5ff649963d2c1cd791
Luis Felipe <202makertv@gmail.com>
2022-03-08 19:33:21 -0300

    Aula 14

--- public/app.js
+++ public/app.js
@@ -1,25 +1,4 @@
-"use strict";
-// Classes
-class Invoice {
-    /*readonly client: string;
-    private details: string;
-    public amount: number;
-  
-    constructor(c: string, d: string, a: number) {
-      this.client = c;
-      this.details = d;
-      this.amount = a;
-    }
-    */
-    constructor(client, details, amount) {
-        this.client = client;
-        this.details = details;
-        this.amount = amount;
-    }
-    format() {
-        return `${this.client} owes $${this.amount} for ${this.details}`;
-    }
-}
+import { Invoice } from "./classes/Invoice.js";
 const invOne = new Invoice("Mario", "Work on the Mario website", 250);
 const invTwo = new Invoice("Luigi", "Work on the Luigi website", 300);
 let invoices = [];
--- src/app.ts
+++ src/app.ts
@@ -1,26 +1,4 @@
-// Classes
-class Invoice {
-  /*readonly client: string;
-  private details: string;
-  public amount: number;
-
-  constructor(c: string, d: string, a: number) {
-    this.client = c;
-    this.details = d;
-    this.amount = a;
-  }
-  */
-
-  constructor(
-    readonly client: string,
-    private details: string,
-    public amount: number,
-  ) { }
-
-  format() {
-    return `${this.client} owes $${this.amount} for ${this.details}`;
-  }
-}
+import { Invoice } from "./classes/Invoice.js";
 
 const invOne = new Invoice("Mario", "Work on the Mario website", 250);
 const invTwo = new Invoice("Luigi", "Work on the Luigi website", 300);
