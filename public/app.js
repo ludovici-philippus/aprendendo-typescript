783af4038a80d3fbf4ea705e88adc6baff1f18bc
Luis Felipe <202makertv@gmail.com>
2022-03-08 19:28:54 -0300

    Aula 13

--- public/app.js
+++ public/app.js
@@ -1,10 +1,20 @@
 "use strict";
 // Classes
 class Invoice {
-    constructor(c, d, a) {
-        this.client = c;
-        this.details = d;
-        this.amount = a;
+    /*readonly client: string;
+    private details: string;
+    public amount: number;
+  
+    constructor(c: string, d: string, a: number) {
+      this.client = c;
+      this.details = d;
+      this.amount = a;
+    }
+    */
+    constructor(client, details, amount) {
+        this.client = client;
+        this.details = details;
+        this.amount = amount;
     }
     format() {
         return `${this.client} owes $${this.amount} for ${this.details}`;
@@ -15,9 +25,11 @@ const invTwo = new Invoice("Luigi", "Work on the Luigi website", 300);
 let invoices = [];
 invoices.push(invOne);
 invoices.push(invTwo);
-invOne.client = "Yoshi";
 invTwo.amount = 400;
 console.log(invoices);
+invoices.forEach(inv => {
+    console.log(inv.format());
+});
 //const form = document.querySelector("form")!;
 const form = document.querySelector(".new-item-form");
 console.log(form.children);
--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,21 @@
 // Classes
 class Invoice {
-  client: string;
-  details: string;
-  amount: number;
+  /*readonly client: string;
+  private details: string;
+  public amount: number;
 
   constructor(c: string, d: string, a: number) {
     this.client = c;
     this.details = d;
     this.amount = a;
   }
+  */
+
+  constructor(
+    readonly client: string,
+    private details: string,
+    public amount: number,
+  ) { }
 
   format() {
     return `${this.client} owes $${this.amount} for ${this.details}`;
@@ -23,10 +30,13 @@ let invoices: Invoice[] = [];
 invoices.push(invOne);
 invoices.push(invTwo);
 
-invOne.client = "Yoshi";
 invTwo.amount = 400;
 
 console.log(invoices);
+
+invoices.forEach(inv => {
+  console.log(inv.format());
+})
 
 //const form = document.querySelector("form")!;
 const form = document.querySelector(".new-item-form") as HTMLFormElement;
